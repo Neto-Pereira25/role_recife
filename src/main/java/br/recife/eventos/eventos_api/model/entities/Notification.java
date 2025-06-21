a0cee7937151ba957a1d2f15e131521ee00f5f39
Neto-Pereira25 <devneto203@gmail.com>
2025-06-21 16:40:52 -0300

    feat(notification): add Notification entity linked to users and events

---
+++ src/main/java/br/recife/eventos/eventos_api/model/entities/Notification.java
@@ -0,0 +1,43 @@
+package br.recife.eventos.eventos_api.model.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String message;
+
+    @Column
+    private LocalDateTime dateDispatch;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    private CommonUser user;
+
+    @ManyToOne
+    @JoinColumn(name = "id_event")
+    private Event event;
+}
