be85abb36559fcbb94627ba425a3c5c728662e0d
Neto-Pereira25 <devneto203@gmail.com>
2025-07-12 16:42:05 -0300

    feat: add footer component to application

---
+++ frontend/assets/js/footerEvents.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const footerContainer = document.createElement("div");
+    fetch("../../../components/footer.html")
+        .then((res) => res.text())
+        .then((html) => {
+            footerContainer.innerHTML = html;
+            document.body.appendChild(footerContainer);
+        });
+});
